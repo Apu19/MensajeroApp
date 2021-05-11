ca929db792f697790ff9e145b9f3ab576cd68b40
Emmanuel <calertios12@gmail.com>
2021-05-11 00:52:03 -0500

    Generar clases y definir métodos

---
+++ MensajeroApp/MensajeroModel/DAL/MensajesDALArchivos.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MensajeroModel.DTO;
+
+namespace MensajeroModel.DAL
+{
+    public class MensajesDALArchivos : IMensajesDAL{
+        
+        private MensajesDALArchivos()
+        {
+
+        }
+        private static IMensajesDAL instancia;
+        public static IMensajesDAL GetInstance()
+        {
+            if(instancia == null)instancia = new MensajesDALArchivos();
+                return instancia;
+        }
+        public List<Mensaje> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Save(Mensaje m)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
