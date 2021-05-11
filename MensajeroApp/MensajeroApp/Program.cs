bd4743d52004cbbba3e502e2cd9b915ac969e76e
Emmanuel <calertios12@gmail.com>
2021-05-11 17:08:05 -0500

    Generar clases para guardar un mensaje.txt

--- MensajeroApp/MensajeroApp/Program.cs
+++ MensajeroApp/MensajeroApp/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using MensajeroModel.DAL;
+using MensajeroModel.DTO;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,8 +10,16 @@ namespace MensajeroApp
 {
     class Program
     {
+        static IMensajesDAL dal = MensajesDALFactory.CreateDAL();
         static void Main(string[] args)
         {
+            Mensaje m = new Mensaje()
+            {
+                Nombre = "Brocacochi",
+                Detalle = "Volvi entero desatado",
+                Tipo = "Aplicación"
+            };
+            dal.Save(m);
         }
     }
 }
--- MensajeroApp/MensajeroModel/DAL/MensajesDALArchivos.cs
+++ MensajeroApp/MensajeroModel/DAL/MensajesDALArchivos.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,7 +9,7 @@ using MensajeroModel.DTO;
 namespace MensajeroModel.DAL
 {
     public class MensajesDALArchivos : IMensajesDAL{
-        
+        private string archivo = Directory.GetCurrentDirectory() + Path.DirectorySeparatorChar + "mensajes.txt";
         private MensajesDALArchivos()
         {
 
@@ -21,12 +22,49 @@ namespace MensajeroModel.DAL
         }
         public List<Mensaje> GetAll()
         {
-            throw new NotImplementedException();
+            List<Mensaje> lista = new List<Mensaje>();
+            try {
+                using (StreamReader reader = new StreamReader(archivo))
+                {
+                    string linea = null;
+                    do
+                    {
+                        if (linea != null)
+                        {
+                            string[] textoArray = linea.Split(';');
+                            Mensaje m = new Mensaje()
+                            {
+                                Nombre = textoArray[0],
+                                Detalle = textoArray[1],
+                                Tipo = textoArray[2]
+                            };
+                            lista.Add(m);
+                        }
+                    } while (linea != null);
+                }
+                
+            }
+            catch (IOException ex)
+            {
+                lista = null;
+            }
+            return lista;
         }
 
         public void Save(Mensaje m)
         {
-            throw new NotImplementedException();
+            try
+            {
+                using (StreamWriter writer = new StreamWriter(archivo, true))
+                {
+                    writer.WriteLine(m);
+                    writer.Flush();
+                }
+
+            }catch(IOException ex)
+            {
+                
+            }
         }
     }
 }
