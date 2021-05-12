bdf8c98d8c667bf9186e9ee078af4b21e230de31
Emmanuel <calertios12@gmail.com>
2021-05-12 00:57:16 -0500

    Agregar funcionalidad para guardar los mensajes

--- MensajeroApp/MensajeroApp/Program.cs
+++ MensajeroApp/MensajeroApp/Program.cs
@@ -11,15 +11,53 @@ namespace MensajeroApp
     class Program
     {
         static IMensajesDAL dal = MensajesDALFactory.CreateDAL();
+        static void IngresarMensaje()
+        {
+            string nombre, detalle;
+            do
+            {
+                Console.WriteLine("Ingrese nombre:");
+                nombre = Console.ReadLine().Trim();
+            }   while (nombre == string.Empty);
+            do
+            {
+                Console.WriteLine("Ingrese detalle:");
+                detalle = Console.ReadLine();
+            } while (detalle == string.Empty);
+
+            Mensaje m = new Mensaje();
+            m.Nombre = nombre;
+            m.Detalle = detalle;
+            m.Tipo = "Aplicación";
+            dal.Save(m);
+        }
+        static void MostrarMensajes()
+        {
+            List<Mensaje> mensajes = dal.GetAll();
+            mensajes.ForEach(m => {
+                Console.WriteLine("Nombre: {0} | Detalle: {1} | Tipo: {2}", m.Nombre, m.Detalle, m.Tipo);
+                });
+        }
+        static bool Menu()
+        {
+            bool continuar = true;
+            Console.WriteLine("Seleccione opcion:");
+            Console.WriteLine("1. Ingresar mensaje");
+            Console.WriteLine("2. Mostrar mensajes");
+            string opcion = Console.ReadLine().Trim();
+            switch(opcion){
+                case "1": IngresarMensaje();
+                    break;
+                case "2": MostrarMensajes();
+                    break;
+                case "0": continuar = false;
+                    break;
+            }
+            return continuar; 
+        }
         static void Main(string[] args)
         {
-            Mensaje m = new Mensaje()
-            {
-                Nombre = "Brocacochi",
-                Detalle = "Volvi entero desatado",
-                Tipo = "Aplicación"
-            };
-            dal.Save(m);
+            while (Menu()) ; 
         }
     }
 }
--- MensajeroApp/MensajeroModel/DAL/MensajesDALArchivos.cs
+++ MensajeroApp/MensajeroModel/DAL/MensajesDALArchivos.cs
@@ -29,6 +29,7 @@ namespace MensajeroModel.DAL
                     string linea = null;
                     do
                     {
+                        linea = reader.ReadLine();
                         if (linea != null)
                         {
                             string[] textoArray = linea.Split(';');
